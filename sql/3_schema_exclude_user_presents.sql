02792eb7d42438d3652083a5c121562902576f25
machida <machida@kmc.gr.jp>
2024-11-24 15:14:46 +0900

    indexは3_ではる

--- sql/3_schema_exclude_user_presents.sql
+++ sql/3_schema_exclude_user_presents.sql
@@ -132,6 +132,10 @@ CREATE TABLE `user_present_all_received_history` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+CREATE INDEX user_id_present_idx ON user_present_all_received_history (
+    user_id, present_all_id
+);
+
 /* ガチャマスタ */
 
 CREATE TABLE `gacha_masters` (
